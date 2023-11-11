1e7109eb798256c065d92e88a787b4b3ba524329
Mahmoud Magdy <mmagdyhafiz2012@gmail.com>
2023-11-11 22:05:53 +0200

    fix photos

--- src/utilities/images.js
+++ src/utilities/images.js
@@ -1,6 +1,6 @@
-import bgDesktop from '/images/bg-header-desktop.svg'
-import bgMobile from '/images/bg-header-mobile.svg'
-import removeIcon from '/images/icon-remove.svg'
+import bgDesktop from 'images/bg-header-desktop.svg'
+import bgMobile from 'images/bg-header-mobile.svg'
+import removeIcon from 'images/icon-remove.svg'
 
 export {
     bgDesktop,
